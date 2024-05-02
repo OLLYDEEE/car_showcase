d1b020a072c43d6b30cee32c7e76996f8e89cedf
innocentdavid <paulinnocent04@gmail.com>
2024-05-02 19:31:03 +0100

    car

---
+++ components/index.ts
@@ -0,0 +1,11 @@
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import Navbar from "./Navbar";
+import Searchbar from "./Searchbar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard"
+import CarDetails from "./CarDetails"
+import Footer from "./Footer";
+import ShowMore from "./ShowMore";
+export { Hero, CustomButton, Navbar, Searchbar,SearchManufacturer, CustomFilter, CarCard,CarDetails, Footer, ShowMore };
